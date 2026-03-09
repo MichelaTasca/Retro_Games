945eff9f899c32eca345162be784eda44a8344d0
Michela Maria Tasca <mtasca04@gmail.com>
2026-03-09 15:06:22 +0100

    style: format code with black

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 """Modulo principale del progetto Retro Games."""
 
+
 def start_game() -> str:
     """Inizializza il menu dei giochi."""
     return "Menu Ready"
+
 
 if __name__ == "__main__":
     print(start_game())
